5c10fbb50616e8de4530a49ef662973d940ba908
unknown <KarimGitHub>
2018-10-09 10:51:17 +0530

    Modules in CRA

--- Day2/day2/src/index.js
+++ Day2/day2/src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from "./components/App";
+
+//ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("app"));//before export
+ReactDOM.render(<App />, document.getElementById("app"));
